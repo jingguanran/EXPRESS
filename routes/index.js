01d43f13e6c4926cc90e52277f73b4490144d144
sugerss <1915492618@qq.com>
2017-07-24 14:43:24 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.post('/post', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 module.exports = router;
